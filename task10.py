f07f6a0a5e04d96e07dd2216152982cf397be54a
javlonbeksaidov-developer <javlonbeksaidov09@gmail.com>
2026-06-09 10:49:07 +0500

    task10.py is done

--- task10.py
+++ task10.py
@@ -7,11 +7,12 @@ Input                   Output
 1, 2, 3, 5, 9, 4, 2     9
 '''
 
-numbers = input("Son kiriting (masalan: 1, 2, 3, 5, 9, 4, 2): ")
+first_num = int(input("Son kiriting: "))
+max_number = first_num
 
-num = []
-for i in numbers:
-    if i.isdigit():
-        num.append(i)
-
-print(max(num))
+for i in range(6):
+    son = int(input("Son kiriting: "))
+    if son > max_number:
+        max_number = son
+        
+print(max_number)
